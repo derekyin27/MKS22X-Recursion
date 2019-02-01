637da00c32a2ee594c27991f3ea1c6fd6921cfd0
Derek <dyin00@stuy.edu>
2019-01-31 22:05:50 -0500

    Started helper

--- recursion.java
+++ recursion.java
@@ -2,6 +2,11 @@ public class recursion{
     public static double sqrt(double n, double tolerance){
       return sqrtH(n, 1, tolerance);
     }
-
-    
+    public static double sqrtH(double n, double guess, double percent){
+      if (Math.abs(guess * (n - guess)) <= percent)
+      return guess;
+      else {
+        guess = (n/guess + guess) / 2
+      }
+    }
 }

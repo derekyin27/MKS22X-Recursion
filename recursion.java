c2b8f17ae62c849a929005c687f78ede87e545f3
Derek <dyin00@stuy.edu>
2019-02-03 16:53:35 -0500

    restarting work

--- recursion.java
+++ recursion.java
@@ -1,14 +1,27 @@
+import java.util.ArrayList;
 public class recursion{
     public static double sqrt(double n, double tolerance){
       return sqrtH(n, 1, tolerance);
     }
-    public static double sqrtH(double n, double guess, double percent){
-      if (Math.abs(guess * (n - guess)) <= percent)
-      return guess;
-      else {
-        guess = (n/guess + guess) / 2;
-        return sqrtH(n, guess, percent);
-      }
+    public static double sqrtH(double n, double guess, double percent){//sqrtH is helper
+
     }
 
+    public static int fib(int n){
+      if (n == 0 || n==1)
+      return n;
+        else return fib(n-1) + fib(n-2);
+    }
+
+    public static ArrayList<Integer> makeAllSums(int n){
+      ArrayList<Integer> finals = new ArrayList<Integer>();//makeAllSUmsH is helper
+      return makeAllSumsH(n, 0, finals);
+    }
+
+
+
+public static void main(String[] args) {
+  System.out.println(makeAllSums(3));
+  System.out.println(sqrt(2.0, 0.000001));
+}
 }
